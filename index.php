783e779bb97fc03200d287c5bafed5cc90448272
Benjamin Sanchez <Linkandcheezer@gmail.com>
2019-04-14 14:53:45 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+include("./header.php");
+/*body*/
+?>
+
+  <nav id='navigation'>
+    <ul>
+      <li>Home</li>
+      <li>Portfolio</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+
+  <header id='header'>
+
+    <h1><span>Benji's</span> Print Shop</h1>
+
+  </header>
+
+  <div id='portfolio-wrapper'>
+
+    <section id='port-images'>
+
+      <img alt='img-1'/>
+      <img alt='img-2'/>
+      <img alt='img-3'/>
+      <img alt='img-4'/>
+      <img alt='img-5'/>
+      <img alt='img-6'/>
+
+    </section>
+
+    <section id='port-aside-images'>
+
+      <img alt='img-1'/>
+      <img alt='img-2'/>
+
+    </section>
+
+    <section id='port-review-wrapper'>
+
+      <div>
+        <img  alt='hero-1'/>
+        <p>Quisque interdum elit vitae tortor congue, a dapibus ex sollicitudin. Praesent viverra elit sed pulvinar dapibus. Phasellus euismod justo nunc,
+          eget tempus libero tincidunt et. Suspendisse potenti. Praesen</p>
+      </div>
+
+      <div>
+        <img alt='hero-2'/>
+        <p>Quisque interdum elit vitae tortor congue, a dapibus ex sollicitudin. Praesent viverra elit sed pulvinar dapibus. Phasellus euismod justo nunc,
+          eget tempus libero tincidunt et. Suspendisse potenti. Praesen</p>
+      </div>
+
+      <div>
+        <img alt='hero-3'/>
+        <p>Quisque interdum elit vitae tortor congue, a dapibus ex sollicitudin. Praesent viverra elit sed pulvinar dapibus. Phasellus euismod justo nunc,
+          eget tempus libero tincidunt et. Suspendisse potenti. Praesen</p>
+      </div>
+
+      <div>
+        <img alt='hero-4'/>
+        <p>Quisque interdum elit vitae tortor congue, a dapibus ex sollicitudin. Praesent viverra elit sed pulvinar dapibus. Phasellus euismod justo nunc,
+          eget tempus libero tincidunt et. Suspendisse potenti. Praesen</p>
+      </div>
+
+      <div>
+        <img alt='hero-5'/>
+        <p>Quisque interdum elit vitae tortor congue, a dapibus ex sollicitudin. Praesent viverra elit sed pulvinar dapibus. Phasellus euismod justo nunc,
+          eget tempus libero tincidunt et. Suspendisse potenti. Praesen</p>
+      </div>
+
+    </section>
+
+  </div>
+
+
+
+
+
+
+
+<?php
+/*body*/
+include("./footer.php");
+?>
