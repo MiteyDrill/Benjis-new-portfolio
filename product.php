ffa18e4af94e806143aa5a4da9d901aa3e1d7193
Benjamin Sanchez <Linkandcheezer@gmail.com>
2019-04-17 19:30:30 -0700

    Simple styling for product page

--- product.php
+++ product.php
@@ -4,6 +4,9 @@ include('main_header.php');
 
 //check if url variable is correct
 if(isset($_GET['id'])){
+
+    //initialize variable
+    $intId = 0;
     
     //checks to see if id is set
     $id = preg_replace('#[^0-9]#i',"", $_GET['id'] );
@@ -11,15 +14,30 @@ if(isset($_GET['id'])){
     // connection parameter
     $connect = mysqli_connect("localhost", "root", "", "products");
 
+    //created a template
+    $sql = "SELECT * FROM product WHERE id=?;";
 
-    $sql = mysqli_query($connect, "SELECT * FROM product WHERE id='$id' LIMIT 1");
+    //prepared statement
+    $stmt = mysqli_stmt_init($connect);
 
-    $productCount = mysqli_num_rows($sql);
+    //prepare the prepared statement
+        //check if the statement works for the template
+    if(!mysqli_stmt_prepare($stmt, $sql)){
+        echo "SQL Statement failed";
+    } else {
 
-    if($productCount > 0){
+        //'bind' parameters to the 'placeholder' (statement, type[s], data)
+        mysqli_stmt_bind_param($stmt, 'i', $id);
 
-        while($row = mysqli_fetch_array($sql)){
+        //run sql query - run paramters inside database
+        mysqli_stmt_execute($stmt);
 
+        //take results from executed statement and put them in a variable
+        $result = mysqli_stmt_get_result($stmt);
+
+        
+
+       $row = mysqli_fetch_assoc($result);
         ?>
         
 
@@ -30,16 +48,9 @@ if(isset($_GET['id'])){
 
 
         <?php
-        }
-
-    } else {
-
-        echo "item does not exist";
-        //redirect
-        header('Location: ./products');
-        exit();
-
     }
+
+    
 } else {
     echo "variable to render information missing";
 }
